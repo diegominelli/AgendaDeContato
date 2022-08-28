84653466574ed99e4ac17f186576eb6d21a49961
Diego Minelli <diego.minelli@hotmail.com>
2022-08-27 22:17:04 -0300

    inicializando o banco de dados

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,13 +1,47 @@
 // ignore: unused_import
 import 'package:sqflite/sqflite.dart';
+// ignore: depend_on_referenced_packages
+import 'package:path/path.dart';
 
+const String contactTable = 'contactTable';
 const String idColumn = 'idColumn';
 const String nameColumn = 'nameColumn';
 const String emailColumn = 'emailColumn';
 const String phoneColumn = 'phoneColumn';
 const String imgColumn = 'imgColumn';
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  // ignore: unused_field
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    // ignore: unused_local_variable
+    final databasesPath = await getDatabasesPath();
+    // ignore: unused_local_variable
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
+}
 
 class Contact {
   int id;
